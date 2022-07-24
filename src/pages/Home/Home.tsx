e13081425ea150bf347ffc11d00340cae299a86b
Apar Awasthi <aparawasthi1@gmail.com>
2022-07-24 20:05:55 +0530

    Updated page content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,8 @@ import imgage from '@assets/test.jpg';
 
 function Home() {
   return (
-    <div>Home
+    <div>
+      <h1 className='text'>Home</h1>
       <img src={imgage}  alt="" />
     </div>
   )
